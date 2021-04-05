73f75101c62838d96dc8d5b9dd30a2f54bd5f8a3
Paulo Trevisolli <paulo.trevisolli@gmail.com>
2021-04-05 12:45:29 -0300

    Commit da primeira aplicação Django. Retornando função de soma.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render, HttpResponse
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos </h1>'.format(nome,idade))
 
+def somar(request, valor1, valor2):
+    valor3 = ( valor1 + valor2 )
+    return HttpResponse('<h1>Soma de  {} e {} é igual à {} </h1>'.format(valor1,valor2,valor3))
+
+
